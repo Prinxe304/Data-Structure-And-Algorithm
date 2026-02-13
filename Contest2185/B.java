a282e6fbd14942de6db442b6b527ace7e546aa2a
Prince Hirpara <124500048+Prinxe304@users.noreply.github.com>
2026-02-13 23:34:42 +0530

    Add files via upload

---
+++ Contest2185/B.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class B{
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+
+    while(t-- > 0){
+      int n = sc.nextInt();
+      int[] arr = new int[n];
+      int maxi = 0;
+
+      for(int i=0; i<n; ++i){
+        arr[i] = sc.nextInt();
+        maxi = Math.max(maxi , arr[i]);
+      }
+
+      int[] pre = new int[n];
+      int sum = arr[0];
+      pre[0] = arr[0];
+
+      for(int i=1; i<n; ++i){
+        pre[i] = Math.max(arr[i] , pre[i-1]);
+        sum += pre[i];
+      }
+
+      System.out.println((maxi * n));
+
+    }
+  }
+  
+}
